14d0082779692a7fbbc1900e712865587733d334
Sebastian <sebastian.abarca.p@gmail.com>
2025-05-10 12:30:03 -0400

    agrega ruta con datos ficticios para testing

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 // server.js
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 4000; // Usa el puerto definido en el entorno o el 3000 por defecto
+const port = 3000; // Puerto en el que estará corriendo tu API
 
+// Middleware para manejar JSON
+app.use(express.json());
+
+// Ruta de ejemplo
 app.get('/', (req, res) => {
-  res.send('¡Hola desde el backend!');
+  res.send('¡Hola desde la API!');
 });
 
+// Iniciar el servidor
 app.listen(port, () => {
-  console.log(`Servidor escuchando en http://localhost:${port}`);
+  console.log(`Servidor API corriendo en http://localhost:${port}`);
+});
+
+app.get('/usuarios', (req, res) => {
+  const usuarios = [
+    { id: 1, nombre: 'Juan' },
+    { id: 2, nombre: 'Ana' },
+  ];
+  res.json(usuarios);  // Responde con un JSON de usuarios
 });
