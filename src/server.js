86a780b39e0f8a1e4599d276e2495580b94afd21
german <ge.weber@duocuc.cl>
2025-05-13 20:43:06 -0400

    integracion de api de servicios de stripe al back

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const usuarioRutas = require('./routes/usuariosRoutes')
+const paymentStripe = require('./routes/paymentStripe')
 require('dotenv').config();
 
 const app = express();
@@ -8,6 +9,7 @@ app.use(express.json());
 
 //RUTAS
 app.use('/usuarios', usuarioRutas);
+app.use('/payment', paymentStripe);
 
 //INICIA LA API
 const PORT = process.env.PORT || 3000;
