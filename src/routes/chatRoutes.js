4f7b914a8d9599f3a0dec98abcfbdeb1d9436911
german <ge.weber@duocuc.cl>
2025-05-17 21:58:37 -0400

    rutas de chat

--- src/routes/chatRoutes.js
+++ src/routes/chatRoutes.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const chatController = require('../controllers/chatController')
 
-router.get('/', chatController.getChats);
-router.get('/:id', chatController.getChatById);
+router.get('/', chatController.getChat);
+router.get('/trabajador/:id', chatController.getChatsByIdTrabajador);
+router.get('/cliente/:id', chatController.getChatsByIdCliente);
 router.get('/mensajes/:id', chatController.getMensajes);
+router.post('/mensajes', chatController.postMensaje);
 
 module.exports = router;
